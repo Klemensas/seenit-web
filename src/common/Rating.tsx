703781882b28e796c940ea3866c47018b57211e1
Klemensas <hello@klemensas.net>
2020-04-13 23:47:16 +0300

    fix: standardize rating value

--- src/common/Rating.tsx
+++ src/common/Rating.tsx
@@ -1,17 +1,23 @@
 import React from 'react';
-import { Icon } from "@blueprintjs/core";
+import { Icon } from '@blueprintjs/core';
 import { IconNames } from '@blueprintjs/icons';
 import ReactRating from 'react-rating';
 
-export default function Rating({ value, className = '' }: { value: number, className?: string }) {
+export default function Rating({
+  value,
+  className = '',
+}: {
+  value: number;
+  className?: string;
+}) {
   return (
     <ReactRating
       readonly
       className={'rating ' + className}
-      initialRating={value / 2}
-      fractions={10}
+      initialRating={value}
+      fractions={2}
       fullSymbol={<Icon icon={IconNames.STAR} color="gold" />}
       emptySymbol={<Icon icon={IconNames.STAR_EMPTY} color="gold" />}
     />
-  )
+  );
 }
